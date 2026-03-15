7e92850e3477496c8cbeb502fa0934884a35b7cc
VIGNESH . G <vignesh19807@gmail.com>
2026-03-15 14:51:33 +0530

    simple interest calculation program

---
+++ 05_simple intrest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class javaq5
+{
+    public static void main(String[] args)
+    {
+       int SI;
+       int p = 1000;
+       int r =5;
+       int t =2;
+       SI=p*r*t/100;
+       System.out.println("Simple Iintrest is :"+SI);
+
+    }
+}
