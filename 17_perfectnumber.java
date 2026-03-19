c8962ecddcd35ff65c0c67e39c7339c1066c6312
VIGNESH . G <vignesh19807@gmail.com>
2026-03-19 23:36:04 +0530

    perfect number checker functionality
    This program checks if a given number is a perfect number by summing its divisors and comparing the sum to the original number.

---
+++ 17_perfectnumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class javaq17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        if (sum == num) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+    }
+}
