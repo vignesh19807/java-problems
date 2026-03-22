12688b93c426ea8516169b52ef362fc6682b486b
VIGNESH G <vignesh19807@gmail.com>
2026-03-22 21:28:40 +0530

    the problems are solved untill 21

--- src/javaq21.java
+++ src/javaq21.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class javaq21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = num;
+
+        // Repeat until single digit
+        while (sum > 9) {
+            int temp = sum;
+            sum = 0;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp = temp / 10;
+            }
+        }
+
+        if (sum == 1) {
+            System.out.println(num + " is a Magic Number");
+        } else {
+            System.out.println(num + " is NOT a Magic Number");
+        }
+
+        sc.close();
+    }
 }
