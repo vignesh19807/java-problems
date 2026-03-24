f2169df54b695705680616ab198eae280672c194
VIGNESH G <vignesh19807@gmail.com>
2026-03-24 13:32:52 +0530

    the third problem was solved -the problem was sum of the array

---
+++ array/src/array3.java
@@ -0,0 +1,15 @@
+public class array3
+{
+    public static void main(String[] args)
+    {
+        int array[]={65,654,58,5,98,5};
+        int sum =0;
+        int i = 0;
+        for(i=0;i<5;i++)
+        {
+
+            sum =sum + array[i];
+        }
+        System.out.println("the sum of the array ="+sum);
+    }
+}
